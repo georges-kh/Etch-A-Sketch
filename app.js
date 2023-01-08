c96a8a4b08ab185cc624e9911204ecfe868d6d0f
Georges Khoueiry <georges.nkhoueiry@gmail.com>
2023-01-08 11:44:23 +0200

    Canvas function
    - covers container div with smaller divs
    - sets the width and height to not leave any empty spaces or overflow
    - initialised the opacity of every internal div (pixel)
    
    Selector function
    
    - called from the mouseover event listener set on pixels
    - changes background depending on which button was clicked
    - changes opacity of pixel if Gradient button chosen
    - calls rainbow function if Rainbow button is clicked
    
    Rainbow function
    
    assigns a random number for the r, g, and b variables and returns them
    to the selector function as a string formatted as an rgb value
    
    Button event listener
    
    changes the option to the id value of the clicked button which is used in
    the selector function
    
    Clear button
    
    - resets the opacity to 1
    - sets background to white to all pixels when clicked
    - resets the option to color

---
+++ app.js
@@ -0,0 +1,76 @@
+const container = document.getElementById("container");
+const pixelsPerSide = 30;
+
+// creates pixels inside container
+function canvas() {
+  for (let i = 0; i < pixelsPerSide**2; i++) {
+    // creates individual pixel
+    const pixel = document.createElement("div");
+    // adds pixel class
+    pixel.classList.add("pixel");
+    // initialises the opacity for the pixel (to be modified with gradient button)
+    pixel.style.opacity = 1;
+    // sets the width and height of each pixel depending on container size and number of pixels per side
+    pixel.style.width = `${+(container.clientWidth) / +pixelsPerSide}px`;
+    pixel.style.height= `${+(container.clientHeight) / +pixelsPerSide}px`;
+
+    container.appendChild(pixel);
+  }
+}
+
+function selector() {
+  // makes selected pixel less opaque and reveals the black background
+  if (option === "gradient") {
+    this.style.opacity -= 0.15;
+
+  } else {
+    // resets the opacity to 1 if it has been modified by the gradient button
+    this.style.opacity = 1;
+
+    // sets the background to black
+    if (option === "color") {
+      this.style.background = "black";
+
+    // sets the background to white
+    } else if (option === "erase") {
+      this.style.background = "white";
+
+    // sets the background of each pixel to random color
+    } else if (option === "rainbow") {
+      this.style.background = rainbow();
+    }
+  }
+}
+
+// return an rgb color with a random value
+function rainbow() {
+  let r = Math.floor(Math.random()*256);
+  let g = Math.floor(Math.random()*256);
+  let b = Math.floor(Math.random()*256);
+  let randomColor = `rgb(${r}, ${g}, ${b})`;
+  return randomColor;
+}
+
+canvas();
+// initialises the option to color so user can start coloring as soon as the page loads
+let option = "color";
+
+// selects all the pixels and run the selector function on each pixel
+const pixels = document.querySelectorAll(".pixel");
+pixels.forEach( (pixel) => pixel.addEventListener("mouseover", selector));
+
+// selects all the buttons and modifies the value of option (to be used in selector)
+const buttons = document.querySelectorAll("button");
+buttons.forEach( (button) => button.addEventListener("click", function() {option = this.getAttribute("id")}));
+
+// selects the clear button and sets the background to all pixels to white when button is clicked
+const clearBtn = document.getElementById("clear");
+clearBtn.onclick = function() {
+  pixels.forEach( function(pixel) {
+    // resets the opacity to 1 if opacity was previously modified with gradient button
+    pixel.style.opacity = 1;
+    pixel.style.background = "white"
+    // resets the option to color so user doesn't have to re-click the color button after clearing
+    option = "color";
+})};
+
